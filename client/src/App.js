fe82b081ed0d7cff419c1c35537ebbbf08405071
NickolaNikolov <71521171+NickolaNikolov@users.noreply.github.com>
2021-03-17 19:15:14 +0200

    Side, menu and main elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import Header from './components/Header/Header';
+import Menu from './components/Menu/Menu';
+import Main from './components/Main/Main';
+
 import style from './App.module.css';
 
 function App() {
   return (
     <div className={style.app}>
-      
       <Header />
+
+      <div className={style.container}>
+        <Menu />
+
+        <Main />
+      </div>
     </div>
   );
 }
---
+++ client/src/components/Main/Main.js
@@ -0,0 +1,14 @@
+import './Main.css';
+
+const Main = () => {
+  return (
+      <main className="main-container">
+          <h1>Soooooooooome heading</h1>
+
+          <p>some posts</p>
+      </main>
+    
+  );
+}
+
+export default Main;
