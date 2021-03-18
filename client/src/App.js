4e08996ca263c9ad9efe94220723260066c26fae
NickolaNikolov <71521171+NickolaNikolov@users.noreply.github.com>
2021-03-18 15:10:26 +0200

    Post and service components

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,56 @@
+import {Component} from 'react';
+
+import * as postService from './services/postService';
+
 import Header from './components/Header/Header';
 import Menu from './components/Menu/Menu';
 import Main from './components/Main/Main';
 
 import style from './App.module.css';
 
-function App() {
-  return (
-    <div className={style.app}>
+// function App() {
+//   return (
+//     <div className={style.app}>
+//       <Header />
+
+//       <div className={style.container}>
+//         <Menu />
+
+//         <Main />
+//       </div>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    postService.getAll()
+      .then(posts => {
+        this.setState({posts})
+      });
+  }
+
+  render() {
+    return (
+      <div className={style.app}>
       <Header />
 
       <div className={style.container}>
         <Menu />
 
-        <Main />
+        <Main posts={this.state.posts}/>
       </div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
